20230829de305643b0c4efbe45667bc14de0230c
Evgeny Gorodetskiy <evsgor@gmail.com>
2022-07-29 17:38:18 +0300

    Upgrade MethaneKit to v0.6.20220729.4

--- Externals/MethaneKit.cmake
+++ Externals/MethaneKit.cmake
@@ -1,8 +1,7 @@
 CPMAddPackage(
     NAME MethaneKit
     GITHUB_REPOSITORY MethanePowered/MethaneKit
-    GIT_TAG 6550a1a4dce5b86a2dee701c7a96b03cbdc3a9ed # last commit in develop branch
-    VERSION 0.6.20220722.6
+    VERSION 0.6.20220729.4
     OPTIONS
         "METHANE_APPS_BUILD_ENABLED OFF"
         "METHANE_TESTS_BUILD_ENABLED OFF"
