98919c76d3d56610281c274fe808ca6262460885
Evgeny Gorodetskiy <evsgor@gmail.com>
2023-06-01 22:00:44 +0300

    Try to fix FastNoise build by disabling x86_64 architecture on MacOS

--- Externals/FastNoise2.cmake
+++ Externals/FastNoise2.cmake
@@ -1,9 +1,3 @@
-if (APPLE)
-    set(FASTSIMD_COMPILE_AARCH64 true)
-    set(FASTSIMD_COMPILE_ARM true)
-    set(FASTSIMD_COMPILE_HAVE_NEON true)
-endif()
-
 CPMAddPackage(
     NAME FastNoise2
     GITHUB_REPOSITORY MethanePowered/FastNoise2
